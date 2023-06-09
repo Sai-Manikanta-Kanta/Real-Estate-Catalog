623d5bc97efa67eb09e8f455692bf20c2d0c8cc5
Sai Manikanta Kanta <you@example.comkanta.saimanikanta@gmail.com>
2023-06-09 20:13:43 +0530

    FESISUP

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+//import AddProperty from './pages/AddProperty'
+//import HomePage from './pages/HomePage'
 
-function App() {
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+// import Profile from './pages/Profile';
+//import ViewPage from './pages/ViewPage';
+//import Update from './pages/Update'
+//import Protected from './Protected'
+const App = () => {
   return (
-    <div className="App">
-      Hello 10X . This is Group 16
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<SignIn/>}/>
+            <Route path='/signup' element={<SignUp/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
